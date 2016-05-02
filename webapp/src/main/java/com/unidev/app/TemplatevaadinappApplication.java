67018928e854564593f0ab62f81002ab9d49b786
Denis O <denis@universal-development.com>
2016-05-02 21:20:27 +0300

    401 error page handling

--- webapp/src/main/java/com/unidev/app/TemplatevaadinappApplication.java
+++ webapp/src/main/java/com/unidev/app/TemplatevaadinappApplication.java
@@ -2,6 +2,10 @@ package com.unidev.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
 public class TemplatevaadinappApplication {
@@ -9,4 +13,12 @@ public class TemplatevaadinappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TemplatevaadinappApplication.class, args);
 	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer servletContainerCustomizer() {
+		return container -> {
+			container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/error/401.html"));
+		};
+	}
+
 }
