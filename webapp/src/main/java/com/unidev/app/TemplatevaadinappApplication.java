4c6a0a407ec939c4e940eb1e555aee6c1657314c
Denis O <denis@universal-development.com>
2016-05-02 20:58:04 +0300

    Regenerated springboot + vaadin project

---
+++ webapp/src/main/java/com/unidev/app/TemplatevaadinappApplication.java
@@ -0,0 +1,12 @@
+package com.unidev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplatevaadinappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplatevaadinappApplication.class, args);
+	}
+}
